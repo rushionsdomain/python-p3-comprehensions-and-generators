45c470f55d2d7fd54359e0de79f9ec9526dda1a4
rushionsdomain <rushionchegge@gmail.com>
2024-09-12 11:12:11 +0300

    Implement return_evens and make_exclamation functions.

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
-
 def return_evens(num_list):
-    pass
+    """Returns a list of even numbers from the given num_list."""
+    return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    """Returns a list of sentences with an exclamation mark appended."""
+    return [sentence + '!' for sentence in sentence_list]
